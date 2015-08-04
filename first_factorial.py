de7d46e012254b3a6e5292387b339bbdf1fa5356
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-04 17:40:53 -0400

    Create first_factorial.py

---
+++ first_factorial.py
@@ -0,0 +1,11 @@
+def FirstFactorial(num): 
+  result=1
+  while num>0:
+    result=result*num
+    num=num-1
+  return result
+print FirstFactorial(raw_input())           
+
+#Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it 
+#(ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
+#Use the Parameter Testing feature in the box below to test your code with different arguments.
