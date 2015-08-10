bb0af379993b9b45e444e510c932b8753f117b8e
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-10 18:30:40 -0400

    Create divistion_stringified.js

---
+++ divistion_stringified.js
@@ -0,0 +1,31 @@
+/*Have the function DivisionStringified(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas. If an answer is only 3 digits long, return the number with no commas (ie. 2 / 3 should output "1"). For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345". 
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.
+*/
+function DivisionStringified(num1,num2) { 
+var results=num1/num2;
+if (results-parseInt(results)>=.5)
+  	results=parseInt(results)+1;
+var resString=parseInt(results)+"";
+  if (resString.length<=3)
+  return resString;
+var commaString="";
+var mod=resString.length%3
+
+if (resString.length>3){
+	for (var count=0; count<resString.length; count++){
+      commaString+=resString[count];
+      if (mod===0)
+        mod+=3;
+        if (count===mod-1 && count<resString.length-1){
+          commaString+=",";
+      		mod+=3;}
+    }
+  return commaString; 
+}
+
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+DivisionStringified(readline());           
