73341c8fed64e9b067fe29a46112c52784bf0d14
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-23 18:07:29 -0400

    Update counting_minutes.js

--- counting_minutes.js
+++ counting_minutes.js
@@ -5,22 +5,22 @@ Use the Parameter Testing feature in the box below to test your code with differ
 
 function CountingMinutesI(str) {
 
-var startTime=str.slice(0,str.indexOf("m")+1);
+var startTime=str.slice(0,str.indexOf("m")+1);// slices out the start time and end times & stores them
 var endTime=str.slice(str.indexOf("-")+1);
-var sHour=parseInt(startTime);
-var eHour=parseInt(endTime);
-if (startTime.indexOf("pm")>0)
+var sHour=parseInt(startTime);//stores the starting hour
+var eHour=parseInt(endTime);//stores the ending hour
+if (startTime.indexOf("pm")>0)//increment times to military time if the time is in pm
     sHour+=12;
 if (endTime.indexOf("pm")>0)
     eHour+=12;    
-var sMinute=parseInt(startTime.slice(startTime.indexOf(":")+1,-2));
+var sMinute=parseInt(startTime.slice(startTime.indexOf(":")+1,-2));//slices out the ending minutes
 var eMinute=parseInt(endTime.slice(endTime.indexOf(":")+1,-2));
-var startInMinutes=(sHour*60)+sMinute;
+var startInMinutes=(sHour*60)+sMinute;//converts the times into total minutes
 var endInMinutes=(eHour*60)+eMinute;
-if (startInMinutes>=endInMinutes)
+if (startInMinutes>=endInMinutes)//if start time is later in the day subtract the total minutes and add 1440 (total minutes in a day) to remove the negative
  	return (endInMinutes-startInMinutes)+1440
   
-  return endInMinutes-startInMinutes
+  return endInMinutes-startInMinutes //subtract total minutes between
          
 }
    
