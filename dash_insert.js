6cf2142ccb2c3f2556a574fa9689b152795a7948
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-23 18:11:49 -0400

    Update dash_insert.js

--- dash_insert.js
+++ dash_insert.js
@@ -3,12 +3,16 @@
 Use the Parameter Testing feature in the box below to test your code with different arguments.
 */
 
+/*function loops through the list and adds the individual characters to the new string. 
+if it encounters an odd number it checks the following number.  if both are odd it inserts 
+a dash and continues the loop */
+
 function DashInsert(str) { 
 var newStr="";
   for (var count=0; count<str.length;count++){
     newStr+=str[count]
     if (count===str.length-1)
-      break
+      break;
     if (str[count]%2!=0 && str[count+1]%2!=0)
       newStr+="-"
       }
