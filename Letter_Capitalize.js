6db9a5ae92a5223bd1e2e02b17f0db305a851b86
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-23 17:00:56 -0400

    Update Letter_Capitalize.js
    comments added

--- Letter_Capitalize.js
+++ Letter_Capitalize.js
@@ -6,16 +6,13 @@ function LetterCapitalize(str) {
 	var newStr = "";
   for (var letter in str){
     if (letter==0)
-      newStr+= str[letter].toUpperCase()
+      newStr+= str[letter].toUpperCase() //add and capitalize the first letter to the new string
     else if (str[letter-1]==" ")
-      newStr+= str[letter].toUpperCase()
+      newStr+= str[letter].toUpperCase() //add and capitalize every letter that appears after a space
     else
-      newStr+=str[letter]
+      newStr+=str[letter] //or just add the letter
       }
   return newStr; 
          
 }
    
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-LetterCapitalize(readline());   
