45cc8a7cb1510721eee903fc3f3c68f7f37f56ec
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-23 17:19:00 -0400

    Update alphabet soup.js

--- alphabet soup.js
+++ alphabet soup.js
@@ -4,12 +4,12 @@ Use the Parameter Testing feature in the box below to test your code with differ
 
 function AlphabetSoup(str) { 
 	var newStr="";
-  for (var counter=0; counter<26;counter++){
+  for (var counter=0; counter<26;counter++){ //loops through each letter of the alphabet using starting points 65 and 97 as character codes that are incremented by the loop
     var start=65;
     var lStart=97;
     start+=counter;
     lStart+=counter;
-  	for (var lCounter=0; lCounter<str.length; lCounter++){ 
+  	for (var lCounter=0; lCounter<str.length; lCounter++){ // checks the original string for whichever alphabet letter the loop has reached and if found adds it to the variable that cotains the alphabetized string
           if (str[lCounter]===String.fromCharCode(start) || str[lCounter]===String.fromCharCode(lStart))
             newStr+=str[lCounter];
         }
