b57be2b047c454dc07ca890ac85ffb11d87bf016
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-30 14:25:10 -0400

    Update letter_changes.js

--- letter_changes.js
+++ letter_changes.js
@@ -3,27 +3,27 @@
 Use the Parameter Testing feature in the box below to test your code with different arguments. */
 
 function LetterChanges(str) { 
-	var alphaArray="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+	var alphaArray="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"; //with no vowels
 	var alpha=[];
   	for (var ltr in alphaArray)
     	alpha.push(alphaArray[ltr]);
-  	var vowels=["a","e","i","o","u"];
+  	var vowels=["a","e","i","o","u"];//only lower case vowels need to be adjusted
   	var newStr="";
-  	for (var letter in str){
-      var newLtr=String.fromCharCode(str.charCodeAt(letter)+1);
-      if (newLtr==="{")
-      	newLtr="a";
-      	
-      else if (newLtr==="[")
-      	newLtr="A";
-        
-      else if (vowels.indexOf(newLtr)>0)
-        newLtr=newLtr.toUpperCase();
- 
-      else if (alpha.indexOf(newLtr)==-1)
-        newLtr=str[letter];
-        
-      newStr+=newLtr;
+  	for (var letter in str){ //selects each letter in the string and adjusts it according to varying conditions
+      		var newLtr=String.fromCharCode(str.charCodeAt(letter)+1); //advance the character by one letter in alpha bet
+		      if (newLtr==="{") //corrects for when the end of alphabet occurs
+		      	newLtr="a";
+		      	
+		      else if (newLtr==="[") //corrects for when the end of alphabet occurs
+		      	newLtr="A";
+		        
+		      else if (vowels.indexOf(newLtr)>0) //upper cases the vowel
+		        newLtr=newLtr.toUpperCase();
+		 
+		      else if (alpha.indexOf(newLtr)==-1) //if the letter needs no adjusting 
+		        newLtr=str[letter];
+		        
+		      newStr+=newLtr; //adds current character into the string
     }
 	return newStr
 
