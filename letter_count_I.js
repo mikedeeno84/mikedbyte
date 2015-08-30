c84c073d908012482a7f9a86312ac2158d9c32ce
mikedeeno84 <mikedeeno84@gmail.com>
2015-08-30 14:39:24 -0400

    Update letter_count_I.js

--- letter_count_I.js
+++ letter_count_I.js
@@ -2,40 +2,41 @@
 
 Use the Parameter Testing feature in the box below to test your code with different arguments.*/
 function LetterCountI(str) { 
-function inWord(inStr){
-	var used=[];
-  	var ltrCt=0;
-  	var repeat=[];
-  	for (var count=0; count<inStr.length;count++){
-      if (used.indexOf(inStr[count])===-1){
-        ltrCt=0;
-        for (var newCount=0;newCount<inStr.length;newCount++){
-          if(inStr[newCount]==inStr[count])
-            ltrCt++;
-        }
-        used.push(inStr[count]);
-        repeat.push(ltrCt);
-      }
-    }
-  var repeated=0;
-  for (count=0;count<repeat.length;count++){
-    if (repeat[count]>1)
-      repeated+=repeat[count]
-      }
-    return repeated;
-}
-  var spltStr=str.split(" ");
+	function inWord(inStr){//counts repeated letters within a single word
+		var used=[];
+	  	var ltrCt=0;
+	  	var repeat=[];
+	  	for (var count=0; count<inStr.length;count++){//loops through each letter  
+		      if (used.indexOf(inStr[count])===-1){//checks to see if we have already counted this letter
+		        ltrCt=0;//resets count to zero
+		      	for (var newCount=0;newCount<inStr.length;newCount++){ //counts appearances of current letter
+		          if(inStr[newCount]==inStr[count])
+		            ltrCt++;//increases the letter count when the inner loop matches the letter set by the outer loop
+		        }
+		        used.push(inStr[count]);//adds to list of checked letters so we don't check the same letter twice
+		        repeat.push(ltrCt);//pushes the total number repeated for particular letter
+		      }
+	    }
+	  var repeated=0;
+	  for (count=0;count<repeat.length;count++){//cycles through the repeat array and total the number of repeated letters
+	    if (repeat[count]>1)
+	      repeated+=repeat[count]
+	      }
+	    return repeated;//returns total number of repeated letters
+	}
+  var spltStr=str.split(" "); //splits the input string into seperate words
   var longCt=0;
   var longWord="";
-  for (var count=0; count<spltStr.length;count++){
+  for (var count=0; count<spltStr.length;count++){ //cycles through the words to determine which has most repeats using inWord function
     if (inWord(spltStr[count])>longCt){
         longCt=inWord(spltStr[count]);
-      	longWord=spltStr[count];
+      	longWord=spltStr[count];//sets the variable to the word with 
   }
   }
-if (longWord==="")
+if (longWord==="")//returns -1 if no word has more that 0 repeated letters
   return -1;
-  return longWord
+  
+  return longWord //returns word with most repeated letters
          
 }
    
