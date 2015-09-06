2baa0dcd0dfed0e4b65ce4d26fdc617682de7b6d
mikedeeno84 <mikedeeno84@gmail.com>
2015-09-06 18:44:27 -0400

    Update longest_word.js

--- longest_word.js
+++ longest_word.js
@@ -4,15 +4,15 @@ Use the Parameter Testing feature in the box below to test your code with differ
 
 
 function LongestWord(sen) { 
-	var newStr="";
+    var newStr="";
     var alpha="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-    for (var letter in sen){
+    for (var letter in sen){ //loops through string to create a new string without non alphabetic characters or spaces
       if (alpha.indexOf(sen[letter])>-1)
         newStr+=sen[letter];
     }
-    var wordList=newStr.split(" ")
-    var bigWord=wordList[0];
- 	for (var word in wordList){
+    var wordList=newStr.split(" ")//splits the new string into an array of words
+    var bigWord=wordList[0];//stores the first word as the longest word
+    for (var word in wordList){//loop through array and if any word length exceeds that of bigWord, bigWord is set to that word
       var currentWord=wordList[word];
       if (currentWord.length >bigWord.length)
         bigWord=wordList[word];
