ebd98581fe4001bb673ea8a3046479a268b02f24
mikedeeno84 <mikedeeno84@gmail.com>
2015-09-06 19:08:27 -0400

    Update number_addition.js

--- number_addition.js
+++ number_addition.js
@@ -4,23 +4,23 @@ Use the Parameter Testing feature in the box below to test your code with differ
 */
 
 function NumberAddition(str) { 
-function aNum(str){
-  var digits=["0","1","2","3","4","5","6","7","8","9","0"];
-  if (digits.indexOf(str)>=0)
-    return true
-  else
-    return false
-    }
+	function aNum(str){//determines whether the input is a number
+	  var digits=["0","1","2","3","4","5","6","7","8","9","0"];
+	  if (digits.indexOf(str)>=0)
+	    return true
+	  else
+	    return false
+	    }
   var total=0;
   var currentNum="";
-  for (var count=0;count<str.length;count++){
-    currentNum="";
-	if (aNum(str[count])===true){
-        while (aNum(str[count])===true){
-      		currentNum+=str[count];
-      		count++;
-    }
-  	total+=parseInt(currentNum);
+  for (var count=0;count<str.length;count++){ //loop through string to find all numbers
+    currentNum="";//current num is set to an empty string
+	if (aNum(str[count])===true){//a number is found
+        	while (aNum(str[count])===true){//loop will stop when the value of aNum is false
+      		currentNum+=str[count];//since we have a number that number is added to the string current number
+      		count++;//the loop counter is incremented to check the next item in the string without exiting the while loop
+    		}//the next item in the string is not a number and the loop terminates
+  	total+=parseInt(currentNum);//parses the last integer pulled from the string and adds it to the total
 }
   }
   return total
