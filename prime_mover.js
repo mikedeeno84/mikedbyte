dcc0285aa9a19c5cf533d2d4d5839fa216ad32c3
mikedeeno84 <mikedeeno84@gmail.com>
2015-09-06 19:27:02 -0400

    Update prime_mover.js

--- prime_mover.js
+++ prime_mover.js
@@ -5,7 +5,7 @@ Use the Parameter Testing feature in the box below to test your code with differ
 function PrimeMover(num) { 
 
   
-function isPrime(n) {
+function isPrime(n) { //functino to determine whether a number is prime
  if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
  if (n%2==0) return (n==2);
  if (n%3==0) return (n==3);
@@ -17,16 +17,15 @@ function isPrime(n) {
  return true;
 }
 var primeArray=[2,3,5];
-for (count=5;count<10000;count+=2){
-  if (count%3 !=0 && count %5 !=0){
+for (count=5;count<10000;count+=2){//loops all odd numbers up to 10^4 for primeness. 
+  if (count%3 !=0 && count %5 !=0){//if number is odd and not divisibe by 3 and 5 checks for prime and adds it to the prime array
     if(isPrime(count)===true)
       primeArray.push(count)
       }
 }
-  return primeArray[num-1]; 
+  return primeArray[num-1];//returns the prime number at the index of n-1/
          
 }
-   
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
 PrimeMover(readline());           
